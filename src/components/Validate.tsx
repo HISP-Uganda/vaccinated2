be52c6463fa883a0384d835613ca70bd4ff9845d
Charles Olupot <olupotcharles@gmail.com>
2021-06-16 16:51:04 +0300

    Add some responsiveness

--- src/components/Validate.tsx
+++ src/components/Validate.tsx
@@ -15,70 +15,70 @@ const Validate = () => {
       {isSuccess && !!data && <Flex direction="column">
         <Flex direction="column">
           <Text bg="yellow.300">Personal Details</Text>
-          <Flex justifyContent="space-between">
+          <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
             <Text fontWeight="bold">Full Name:</Text>
-            <Text pl="10px">{data[NAME_ATTRIBUTE]}</Text>
+            <Text pl={[null, null, "10px"]}>{data[NAME_ATTRIBUTE]}</Text>
           </Flex>
-          <Flex justifyContent="space-between">
+          <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
             <Text fontWeight="bold">Vaccination Card No:</Text>
-            <Text pl="10px">{data[VACCINATION_CARD_NO]}</Text>
+            <Text pl={[null, null, "10px"]}>{data[VACCINATION_CARD_NO]}</Text>
           </Flex>
-          <Flex justifyContent="space-between">
+          <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
             <Text fontWeight="bold">Registration ID:</Text>
-            <Text pl="10px">{data[NIN_ATTRIBUTE] || data[OTHER_ID]}</Text>
+            <Text pl={[null, null, "10px"]}>{data[NIN_ATTRIBUTE] || data[OTHER_ID]}</Text>
           </Flex>
-          {/* <Flex justifyContent="space-between">
+          {/* <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
             <Text>Certificate No:</Text>
-            <Text pl="10px">CHarles Olupot</Text>
+            <Text pl={[null, null, "10px"]}>CHarles Olupot</Text>
           </Flex> */}
         </Flex>
         <Flex direction="column" mt="10">
           <Text bg="yellow.300" mb="10px">Vaccination Details</Text>
-          <SimpleGrid columns={2} spacing="10">
+          <SimpleGrid columns={[1, 1, 2]} spacing="10">
             <Flex direction="column" border="1px solid gray" p="5px">
               <Text bg="gray.200">Dose1</Text>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Date:</Text>
-                <Text pl="10px">{new Intl.DateTimeFormat('fr').format(Date.parse(data["0"].eventDate))}</Text>
+                <Text pl={[null, null, "10px"]}>{new Intl.DateTimeFormat('fr').format(Date.parse(data["0"].eventDate))}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Batch No:</Text>
-                <Text pl="10px">{data["0"].Yp1F4txx8tm}</Text>
+                <Text pl={[null, null, "10px"]}>{data["0"].Yp1F4txx8tm}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Vaccine:</Text>
-                <Text pl="10px">{data["0"].bbnyNYD1wgS}</Text>
+                <Text pl={[null, null, "10px"]}>{data["0"].bbnyNYD1wgS}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">MFG:</Text>
-                <Text pl="10px">{data["0"].rpkH9ZPGJcX}</Text>
+                <Text pl={[null, null, "10px"]}>{data["0"].rpkH9ZPGJcX}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Facility:</Text>
-                <Text pl="10px">{data["0"].orgUnitName}</Text>
+                <Text pl={[null, null, "10px"]}>{data["0"].orgUnitName}</Text>
               </Flex>
             </Flex>
             <Flex direction="column" border="1px solid gray" p="5px">
               <Text bg="gray.200">Dose2</Text>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Date:</Text>
-                <Text pl="10px">{new Intl.DateTimeFormat('fr').format(Date.parse(data["1"].eventDate))}</Text>
+                <Text pl={[null, null, "10px"]}>{new Intl.DateTimeFormat('fr').format(Date.parse(data["1"].eventDate))}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Batch No:</Text>
-                <Text pl="10px">{data["1"].Yp1F4txx8tm}</Text>
+                <Text pl={[null, null, "10px"]}>{data["1"].Yp1F4txx8tm}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Vaccine:</Text>
-                <Text pl="10px">{data["1"].bbnyNYD1wgS}</Text>
+                <Text pl={[null, null, "10px"]}>{data["1"].bbnyNYD1wgS}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">MFG:</Text>
-                <Text pl="10px">{data["1"].rpkH9ZPGJcX}</Text>
+                <Text pl={[null, null, "10px"]}>{data["1"].rpkH9ZPGJcX}</Text>
               </Flex>
-              <Flex justifyContent="space-between">
+              <Flex justifyContent="space-between" direction={["column", "column", "row"]}>
                 <Text fontWeight="bold">Facility:</Text>
-                <Text pl="10px">{data["1"].orgUnitName}</Text>
+                <Text pl={[null, null, "10px"]}>{data["1"].orgUnitName}</Text>
               </Flex>
             </Flex>
           </SimpleGrid>
